6a31885f355843205cb50e6c9a823852b8efe466
Camila Maron <camila.maron93@gmail.com>
2025-10-17 01:28:27 -0300

    Created 03-functions-directives project

---
+++ 03-functions-directives/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      spacing: {
+        128: '32rem',
+      },
+      height: {
+        128: '32rem',
+      },
+    },
+  },
+  plugins: [],
+};
